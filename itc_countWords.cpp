eec33bf3c20038b27aa80a8d04d6e7f2d93617e7
gevorik3991 <92302057+gevorik3991@users.noreply.github.com>
2021-11-18 20:05:43 +0300

    Update itc_countWords.cpp

--- itc_countWords.cpp
+++ itc_countWords.cpp
@@ -2,11 +2,14 @@
 
 int itc_countWords(string str)
 {
-	unsigned char prev;
 	long long len = itc_len(str), kol;
-	if (((str[0] >= 'A') && (str[0] <= 'Z')) || ((str[0] >= 'a') && (str[0] <= 'z'))) kol = 1;
+	if (str == "") return 0;
+	else if (((str[0] >= 'A') && (str[0] <= 'Z')) || ((str[0] >= 'a') && (str[0] <= 'z'))) kol = 1;
 	else kol = 0;
 	for (int i = 1; i < len; i++) {
+		if (!(((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z'))) && (str[i] != ' ')){
+			kol -= 2;
+		}
 		if (!(((str[i - 1] >= 'A') && (str[i - 1] <= 'Z')) || ((str[i - 1] >= 'a') && (str[i - 1] <= 'z'))) && (((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')))) {
 			kol++;
 		}
